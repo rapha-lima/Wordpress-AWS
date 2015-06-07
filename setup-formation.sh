745e1b99c6c3b987d359c8025eaf1c4e192aa332
Raphael Lima <raphael.lima@adtsys.com>
2015-06-07 19:09:27 -0300

    Fix bug

--- setup-formation.sh
+++ setup-formation.sh
@@ -3,18 +3,18 @@
 read -p "Insert Access Key: " AWS_ACCESS_KEY
 read -p "Insert Secret Key: " AWS_SECRET_KEY
 
-export AWS_ACCESS_KEY
-export AWS_SECRET_KEY
+export AWS_ACCESS_KEY=$AWS_ACCESS_KEY
+export AWS_SECRET_KEY=$AWS_SECRET_KEY
 
-yum install wget unzip -y
+sudo yum install wget unzip -y
 
 if ! type aws >> /dev/null ; then
   echo "There is no AWS Cli packaged installed. Let's install it."
-  wget --quiet https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -O /tmp/awscli-bundle.zip && unzip -qo /tmp/awscli-bundle.zip -d /tmp/ && /tmp/awscli-bundle/install -i /usr/local/aws -b /usr/bin/aws
+  sudo wget --quiet https://s3.amazonaws.com/aws-cli/awscli-bundle.zip -O /tmp/awscli-bundle.zip && unzip -qo /tmp/awscli-bundle.zip -d /tmp/ && /tmp/awscli-bundle/install -i /usr/local/aws -b /usr/bin/aws
 fi
 
-aws ec2 create-key-pair --key-name master --region sa-east-1 --query 'KeyMaterial' --output text > master.pem
+aws ec2 create-key-pair --key-name master --region sa-east-1 --query 'KeyMaterial' --output text > wordpress.pem
 
 chmod 400 master.pem
 
-aws cloudformation create-stack --stack-name Infra-base --template-body file://aws//templates//Infra-Base.json --region sa-east-1
+aws cloudformation create-stack --stack-name Infra-base --template-body file://aws//templates//Infra-Base.json --region sa-east-1 --capabilities CAPABILITY_IAM
