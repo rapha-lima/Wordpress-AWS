f7c9b9f569f1036bc2670acc525dd24aa6112aba
Raphael Lima <raphael.lima@adtsys.com>
2015-06-09 01:33:30 -0300

    Fix bug

--- salt/masterless/files/attach_instance-asg.sh
+++ salt/masterless/files/attach_instance-asg.sh
@@ -2,4 +2,4 @@
 
 instanceid=$(curl http://169.254.169.254/latest/meta-data/instance-id)
 asg=$(aws autoscaling describe-auto-scaling-groups --region sa-east-1 | grep "AutoScalingGroupName" | grep Infra-base | cut -d '"' -f 4)
-aws autoscaling attach-instances --instance-ids $instanceid --auto-scaling-group-name $asg --region sa-east-1
+aws autoscaling attach-instances --instance-ids $instanceid --auto-scaling-group-name $asg --region sa-east-1 > /tmp/attach.log
